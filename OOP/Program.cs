319ea9010166675290073704b03845ea3c4f9df0
Nikolai <niko665a@zbc.dk>
2021-02-23 08:26:38 +0100

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square sqr = new Square(123);
+
+            Console.WriteLine($"Omkreds: {sqr.CalcPerimeter()}");
+            Console.WriteLine($"Areal: {sqr.CalcArea()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Square
+    {
+        public double A { get; set; }
+
+        public Square(double a)
+        {
+            this.A = a;
+        }
+        
+        public double CalcPerimeter()
+        {
+            return 4 * A;
+        }
+
+        public double CalcArea()
+        {
+            return A * A;
+        }
+    }
+}
